af5fb2fe5a17aa527f1eddca6172ad377d684896
Sergey Smolin <soees84@gmail.com>
2018-02-12 20:29:20 +0500

    Update mlab dbname handling

--- db/index.js
+++ db/index.js
@@ -3,7 +3,15 @@ const MongoClient = require('mongodb').MongoClient;
 let db;
 
 const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/lolstrats';
-const dbname = process.env.MONGODB_URI ? 'heroku_v09sc7ks' : 'lolstrats';
+
+let dbname;
+
+if (process.env.MONGODB_URI) {
+  const match = /.*mlab.com:[0-9]*\/(.*)/g.exec(process.env.MONGODB_URI);
+  dbname = match[1];
+} else {
+  dbname = 'lolstrats';
+}
 
 MongoClient.connect(url, function(err, dbHandle) {
   db = dbHandle.db(dbname);
